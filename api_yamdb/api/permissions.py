6bcd2e0eb02829080ec7ba6a8d3b21398df86183
DNKer <dnker@mail.ru.com>
2023-02-12 14:31:19 +0500

    "Merge with develop"

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import get_user_model
 from rest_framework import permissions
 
 
@@ -20,10 +19,38 @@ class ChangeAdminOnly(permissions.BasePermission):
     """Изменение доступно только администратору."""
 
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_admin
+        return (
+            request.user.is_authenticated
+            and (request.user.is_superuser or request.user.role == 'admin')
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+
+            request.user.is_authenticated
+            and (request.user.is_superuser or request.user.role == 'admin')
+        )
 
 
 class StaffOrReadOnly(permissions.BasePermission):
+    """Администратор или только чтение."""
+
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or (request.user.is_authenticated and request.user.role == 'admin')
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+
+            request.method in permissions.SAFE_METHODS
+            or (request.user.is_authenticated and request.user.role == 'admin')
+        )
+
+
+class AuthorOrStaffOrReadOnly(permissions.BasePermission):
+    """Автор, модератор, админ или только чтение."""
 
     def has_permission(self, request, view):
         return (
@@ -34,36 +61,7 @@ class StaffOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or (request.user.is_authenticated and request.user.role == 'admin')
+            or obj.author == request.user
+            or request.user.is_superuser
+            or request.user.role in ('moderator', 'admin')
         )
-
-
-class AuthorOrReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return (
-            (request.method in permissions.SAFE_METHODS
-             or request.user.is_authenticated)
-        ) or (request.user.is_superuser or request.user.role == 'admin')
-
-    def has_object_permission(self, request, view, obj):
-        return (
-            (request.method in permissions.SAFE_METHODS
-             or obj.author == request.user)
-        ) or (request.user.is_superuser or request.user.role == 'admin')
-
-
-class ModeratorReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return (
-
-            request.user.is_authenticated
-            or request.user.is_superuser or request.user.role != 'user'
-        )
-
-    def has_object_permission(self, request, view, obj):
-        return (
-
-            obj.author == request.user
-            or request.user.is_superuser or request.user.role != 'user')
