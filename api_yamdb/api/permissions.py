ea7e7ef3f5ae848715b9158fbbea91991705756c
DNKer <dnker@mail.ru.com>
2023-02-12 14:09:51 +0500

    Merge to develop

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -43,7 +43,6 @@ class StaffOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-
             request.method in permissions.SAFE_METHODS
             or (request.user.is_authenticated and request.user.role == 'admin')
         )
